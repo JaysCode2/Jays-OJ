c8c101abf42c205b89c97f3b0027c89548182729
JaysCode <jayscode@user.noreply.gitee.com>
2023-10-04 22:13:03 +0800

    debug

--- src/main/java/com/yupi/yuoj/config/MyBatisPlusConfig.java
+++ src/main/java/com/yupi/yuoj/config/MyBatisPlusConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * MyBatis Plus 配置
  *
- * @author https://github.com/liyupi
+ * @author https://github.com/JaysCode2
  */
 @Configuration
 @MapperScan("com.yupi.yuoj.mapper")
