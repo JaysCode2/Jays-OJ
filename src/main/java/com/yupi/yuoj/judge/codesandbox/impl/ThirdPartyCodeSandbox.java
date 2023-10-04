e98d44bc6d1a6f68e0b3c81d23c38b4d646a454c
JaysCode <jayscode@user.noreply.gitee.com>
2023-10-04 20:59:48 +0800

    original version

---
+++ src/main/java/com/yupi/yuoj/judge/codesandbox/impl/ThirdPartyCodeSandbox.java
@@ -0,0 +1,19 @@
+package com.yupi.yuoj.judge.codesandbox.impl;
+
+import com.yupi.yuoj.judge.codesandbox.CodeSandbox;
+import com.yupi.yuoj.judge.codesandbox.model.ExecuteCodeRequest;
+import com.yupi.yuoj.judge.codesandbox.model.ExecuteCodeResponse;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 第三方代码沙箱（调用网上现成的代码沙箱）
+ * 这里用juge0
+ */
+@Slf4j
+public class ThirdPartyCodeSandbox implements CodeSandbox {
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        log.info("以下是使用了juge0-API");
+        return null;
+    }
+}
